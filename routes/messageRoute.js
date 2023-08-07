363f8fc660fd178a77b17a2200f23b8b59377a2c
Siyun Feng <siyunf21@gmail.com>
2023-08-06 21:29:18 -0400

    added messageRoute GET request to handle fetch basic chat page info by passing chat id

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -3,23 +3,54 @@ const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
 const User = require('../models/UserModel');
+const Chat = require('../models/ChatModel');
 
 router.get('/', async (req, res, next) => {
-  let payload = {
-    pageTitle: 'Inbox',
-    loggedInUser: req.session.user,
-    loggedInUserJS: JSON.stringify(req.session.user),
-  };
-  res.status(200).render('inboxPage', payload);
+  try {
+    let payload = {
+      pageTitle: 'Inbox',
+      loggedInUser: req.session.user,
+      loggedInUserJS: JSON.stringify(req.session.user),
+    };
+    res.status(200).render('inboxPage', payload);
+  } catch (error) {
+    console.log('messageRoute / GET request error: ', error);
+  }
 });
 
 router.get('/new', async (req, res, next) => {
-  let payload = {
-    pageTitle: 'New Message',
-    loggedInUser: req.session.user,
-    loggedInUserJS: JSON.stringify(req.session.user),
-  };
-  res.status(200).render('newMessage', payload);
+  try {
+    let payload = {
+      pageTitle: 'New Message',
+      loggedInUser: req.session.user,
+      loggedInUserJS: JSON.stringify(req.session.user),
+    };
+    res.status(200).render('newMessage', payload);
+  } catch (error) {
+    console.log('messageRoute /new GET request error: ', error);
+  }
+});
+
+router.get('/:chatId', async (req, res, next) => {
+  try {
+    const userId = req.session.user._id;
+    const { chatId } = req.params;
+
+    let chat = await Chat.findOne({
+      _id: chatId,
+      users: { $elemMatch: { $eq: userId } },
+    }).populate('users');
+
+    let payload = {
+      pageTitle: 'Chat',
+      loggedInUser: req.session.user,
+      loggedInUserJS: JSON.stringify(req.session.user),
+      chat: chat,
+    };
+    res.status(200).render('chatPage', payload);
+  } catch (error) {
+    console.log('messageRoute /:chatId GET request error: ', error);
+  }
 });
 
 module.exports = router;
