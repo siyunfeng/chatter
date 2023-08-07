1c5a44940687e91c052dc9367a15265a15e4d506
Siyun Feng <siyunf21@gmail.com>
2023-08-07 02:44:46 -0400

    added checking if the chat exists, handled error message for if chat does not exist or not by loggedin user to secure other users chat, updated get to the direct message chat from user profile page and create a new chat if it did not exist yet.

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const User = require('../models/UserModel');
 const Chat = require('../models/ChatModel');
 
@@ -35,22 +36,65 @@ router.get('/:chatId', async (req, res, next) => {
   try {
     const userId = req.session.user._id;
     const { chatId } = req.params;
+    const isValidId = mongoose.isValidObjectId(chatId);
+    const erroMessage =
+      'Chat does not exist or you do not have permission to view it.';
+
+    let payload = {
+      pageTitle: 'Chat',
+      loggedInUser: req.session.user,
+      loggedInUserJS: JSON.stringify(req.session.user),
+    };
+
+    if (!isValidId) {
+      payload.errorMessage = erroMessage;
+      return res.status(200).render('chatPage', payload);
+    }
 
     let chat = await Chat.findOne({
       _id: chatId,
       users: { $elemMatch: { $eq: userId } },
     }).populate('users');
 
-    let payload = {
-      pageTitle: 'Chat',
-      loggedInUser: req.session.user,
-      loggedInUserJS: JSON.stringify(req.session.user),
-      chat: chat,
-    };
+    if (chat === null) {
+      const userFound = await User.findById(chatId);
+
+      if (userFound !== null) {
+        chat = await getChatByUserId(userFound._id, userId);
+      }
+    }
+
+    if (chat === null) {
+      payload.errorMessage = erroMessage;
+    } else {
+      payload.chat = chat;
+    }
+
     res.status(200).render('chatPage', payload);
   } catch (error) {
     console.log('messageRoute /:chatId GET request error: ', error);
   }
 });
+
+const getChatByUserId = (loggedInUserId, userToChatWithId) => {
+  const chat = Chat.findOneAndUpdate(
+    {
+      isGroupChat: false,
+      users: {
+        $size: 2,
+        $all: [
+          { $elemMatch: { $eq: new mongoose.Types.ObjectId(loggedInUserId) } },
+          {
+            $elemMatch: { $eq: new mongoose.Types.ObjectId(userToChatWithId) },
+          },
+        ],
+      },
+    },
+    { $setOnInsert: { users: [loggedInUserId, userToChatWithId] } },
+    { new: true, upsert: true }
+  ).populate('users');
+
+  return chat;
+};
 
 module.exports = router;
