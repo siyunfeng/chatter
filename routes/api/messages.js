2629ffc0497b921854a04071c9865359304349f1
Siyun Feng <siyunf21@gmail.com>
2023-08-09 22:08:06 -0400

    created /api/messages route, added a POST route to handle creating a new message

---
+++ routes/api/messages.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const bodyParser = require('body-parser');
+const User = require('../../models/UserModel');
+const Post = require('../../models/PostModel');
+const Chat = require('../../models/ChatModel');
+const Message = require('../../models/MessageModel');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// create a new chat page
+router.post('/', async (req, res, next) => {
+  try {
+    const { content, chatId } = req.body;
+
+    if (!content || !chatId) {
+      console.log('invalid data passed into request');
+      return res.sendStatus(400);
+    }
+
+    let newMessage = {
+      sender: req.session.user._id,
+      content: content,
+      chat: chatId,
+    };
+
+    const message = await Message.create(newMessage);
+    res.status(201).send(message);
+  } catch (error) {
+    console.log('/api/messages route POST request error: ', error);
+    res.sendStatus(400);
+  }
+});
+
+module.exports = router;
