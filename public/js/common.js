db8e140fc3328b5d40f42be6eb0b0ef412c7d36e
Siyun Feng <siyunf21@gmail.com>
2023-08-22 19:21:33 -0400

    transferred inbox messages handler functions to common.js

--- public/js/common.js
+++ public/js/common.js
@@ -734,6 +734,8 @@ const messageReceived = (newMessage) => {
   updateBadge('messages');
 };
 
+// Notifications handlers
+
 const markNotificationAsRead = (notificationId = null, callback = null) => {
   if (callback === null) {
     callback = () => location.reload();
@@ -835,4 +837,51 @@ const notificationPopup = (data) => {
   element.hide().prependTo('#notificationList').slideDown('fast');
 
   setTimeout(() => element.fadeOut(600), 5000);
+};
+
+// Messages handlers
+
+const createChatHtml = (chatData) => {
+  let chatName = getChatName(chatData);
+  let chatImage = getChatImageElements(chatData);
+  let latestMessage = getLatestMessage(chatData.latestMessage);
+
+  return `<a href='/messages/${chatData._id}' class='resultListItem'>
+            ${chatImage}
+            <div class='resultsDetailsContainer ellipsis'>
+              <span class='heading ellipsis'>${chatName}</span>
+              <span class='subText ellipsis'>${latestMessage}</span>
+            </div>
+          </a>`;
+};
+
+const getChatImageElements = (chatData) => {
+  let usersToChatWith = getUsersToChatWith(chatData.users);
+
+  let groupChatClass = '';
+  let chatImage = getUserChatImageElement(usersToChatWith[0]);
+
+  if (usersToChatWith.length > 1) {
+    groupChatClass = 'groupChatImage';
+    chatImage += getUserChatImageElement(usersToChatWith[1]);
+  }
+
+  return `<div class='resultsImageContainer ${groupChatClass}'>${chatImage}</div>`;
+};
+
+const getUserChatImageElement = (user) => {
+  if (!user || !user.profileImage) {
+    return alert('User or user profile image not found');
+  }
+
+  return `<img src='${user.profileImage}' alt='user profile image' />`;
+};
+
+const getLatestMessage = (latestMessage) => {
+  if (latestMessage) {
+    let sender = latestMessage.sender;
+    return `${sender.firstName} ${sender.lastName}: ${latestMessage.content}`;
+  }
+
+  return 'New chat';
 };
